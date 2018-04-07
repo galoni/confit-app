05e385046b0966e9f7c6b3826153bd018390b6d0
NivBraz <nivbraz90@gmail.com>
2018-04-08 01:51:31 +0300

    create program

--- src/app/new-conf-sessions/new-conf-sessions.component.ts
+++ src/app/new-conf-sessions/new-conf-sessions.component.ts
@@ -18,12 +18,18 @@ export class NewConfSessionsComponent implements OnInit {
   timeTable: number[] = [];
   selectedDay:number;
   startTime: number= 9;
+  program: ConfSession[]= [];
 
   constructor(private newConfService: NewConfService,
               private router: Router, private r:ActivatedRoute) { }
 
   ngOnInit() {
     this.confId = localStorage.getItem('confId');
+    if(!this.confId) {
+      this.confId = "5ac93dab41bbaa14700fb08c";
+    }
+    this.confSessions = JSON.parse(localStorage.getItem('confSessions'));
+    console.log("conf session 1: " + JSON.stringify(this.confSessions));
     this.numDays = +(localStorage.getItem('confDuration'));
     if(this.numDays){
       console.log("numDays: " + this.numDays);
@@ -31,13 +37,19 @@ export class NewConfSessionsComponent implements OnInit {
     else{
       this.numDays = 3;
     }
-    for(let i=0; i< this.numDays; i++){
-      this.timeTable[i] = this.startTime;
-      this.confSessions[i] = [];
+    if (this.confSessions == undefined){
+      this.confSessions = [];
+      for(let i=0; i< this.numDays; i++){
+        this.timeTable[i] = this.startTime;
+        this.confSessions[i] = [];
+      }
+    }
+    else{
+      this.timeTable = JSON.parse(localStorage.getItem("timeTable"));
     }
   }
   onChange(day){
-    console.log("day: " + day);
+    // console.log("day: " + day);
     this.selectedDay = day;
   }
   counter(i: number) {
@@ -52,29 +64,37 @@ export class NewConfSessionsComponent implements OnInit {
       }
       this.timeTable[sess.dayNum-1] -= sess.duration;
       this.confSessions[sess.dayNum-1].splice(index, 1);
+      localStorage.setItem("confSessions", JSON.stringify(this.confSessions));
+      localStorage.setItem("timeTable", JSON.stringify(this.timeTable));
     }
   }
   createSession(form: NgForm) {
-    console.log("time table: "+ this.timeTable);
+    // console.log("time table: "+ this.timeTable);
     this.data.name = form.value.name;
     this.data.session_type = form.value.session_type;
     this.data.duration = form.value.duration;
     this.data.dayNum = this.selectedDay+1;
     this.data.time = this.timeTable[this.selectedDay];
     this.timeTable[this.selectedDay] = this.timeTable[this.selectedDay] + this.data.duration;
-    console.log("dynum: " + this.selectedDay);
-    let insertData = Object.assign({}, this.data);
-    this.confSessions[this.selectedDay].push(insertData);
+    // console.log("dynum: " + this.selectedDay);
+    let sess = new ConfSession(this.data.name, this.data.session_type, this.data.duration, this.data.dayNum, this.data.time);
+    // console.log("sess: " + JSON.stringify(sess));
+    // let insertData = Object.assign({}, this.data);
+    this.confSessions[this.selectedDay].push(sess);
     console.log("conf session: "+ JSON.stringify(this.confSessions));
+    localStorage.setItem("confSessions", JSON.stringify(this.confSessions));
+    localStorage.setItem("timeTable", JSON.stringify(this.timeTable));
   }
-  // addManyLectures(){
-  //   console.log(this.confLectures);
-  //   this.newConfService.addManyLectures(this.confLectures, this.confId).then((conf) =>{
-  //     console.log(conf);
-  //     localStorage.setItem('lectures', JSON.stringify(this.lectures));
-  //     localStorage.setItem('confLectures', JSON.stringify(this.confLectures));
-  //     this.router.navigate(["../sessions"], { relativeTo: this.r });
-  //   });
-  // }
+  createProgram(){
+    for(let i = 0; i < this.confSessions.length; i++){
+      for(let j = 0; j < this.confSessions[i].length; j++){
+        this.program.push(this.confSessions[i][j]);
+      }
+    }
+    this.newConfService.createProgram(this.program, this.confId).then((conf) =>{
+      console.log(conf);
+      this.router.navigate(["../sessions"], { relativeTo: this.r });
+    });
+  }
 
 }
