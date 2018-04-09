302c59aacba3eae591be3736cc5a5bd808a18eeb
NivBraz <nivbraz90@gmail.com>
2018-04-09 20:52:20 +0300

    Update lecture.ts

--- src/app/models/lecture.ts
+++ src/app/models/lecture.ts
@@ -1,7 +1,7 @@
 export class Lecture {
   public _id: string;
 
-  constructor(name: String, lecturer_name: String,
-              duration: Number, description: String,
-              ratings: Number){}
+  constructor(public name: string, public lecturer_name: string,
+              public duration: number, public description: string,
+              public ratings: number, public topics: string[]){}
 }
