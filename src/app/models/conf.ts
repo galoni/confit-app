fddb2a75c360c77497981d27d69e6641edf1306b
NivBraz <nivbraz90@gmail.com>
2018-04-10 19:46:19 +0300

    visitor Num

--- src/app/models/conf.ts
+++ src/app/models/conf.ts
@@ -6,8 +6,9 @@ export class Conf {
   public program: ConfSession[];
   public visitors: string[];
   public topics: string[];
+  public lectures: Lecture[];
   constructor(public name: string, public type: string,
-              public logo: string, public start_date: string, public lectures: Lecture[],
+              public logo: string, public start_date: string,
               public duration: number, public location: string, public audience: string) {
     this.lectures = [];
   }
