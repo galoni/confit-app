1bf70adac753f55556dcc732bbaf490f8ee2bfaf
nofarginon <nofar.ginon1@gmail.com>
2018-05-05 16:22:57 +0300

    'updatingRouting'

--- src/app/visitor-landing/visitor-landing.component.ts
+++ src/app/visitor-landing/visitor-landing.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Visitor } from "../models/visitor";
+import { myConfService } from "../services/myConf.service";
+import { NgForm } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { Conf } from "../models/conf";
 
 @Component({
   selector: 'app-visitor-landing',
@@ -6,10 +11,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./visitor-landing.component.css']
 })
 export class VisitorLandingComponent implements OnInit {
-
-  constructor() { }
+  visitor: Visitor;
+  visitorId:string;
+  panelOpenState: boolean = false;
+  
+  constructor(private myConfService: myConfService,
+    private router: Router, private r: ActivatedRoute) { }
 
   ngOnInit() {
+        this.visitor = new Visitor("linkedin", "education", "occupation", "qr_code");
+        this.visitorId = localStorage.getItem('visitorId');
+        this.myConfService.getVisitorById(this.visitorId).then((visitor) => {
+          if (visitor) {
+            this.visitor=visitor;
+            console.log(this.visitor);
+          }
+        });
   }
 
 }
