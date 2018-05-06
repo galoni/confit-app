b50ea75baa2b777dcdc0b7d67d0a4f5ae3f3754e
NivBraz <nivbraz90@gmail.com>
2018-05-06 08:52:10 +0300

    show program-dialog

--- src/app/new-conf-program-show/new-conf-program-show.component.ts
+++ src/app/new-conf-program-show/new-conf-program-show.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { NewConfService } from "../services/newConf.service";
-import { ConfSession } from "../models/confSession";
-import {ActivatedRoute, Router} from "@angular/router";
-import {Subscription} from "rxjs/Subscription";
-import {Conf} from "../models/conf";
+import { NewConfService } from '../services/newConf.service';
+import { ConfSession } from '../models/confSession';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Subscription} from 'rxjs/Subscription';
+import {Conf} from '../models/conf';
+import {ManagerService} from '../services/manager.service';
 
 @Component({
   selector: 'app-new-conf-program-show',
@@ -12,65 +13,78 @@ import {Conf} from "../models/conf";
 })
 export class NewConfProgramShowComponent implements OnInit {
   data: any = [];
-  topicsLectures:any = [];
-  subscription:Subscription;
-  @Input() fConf:Conf;
+  topicsLectures: any = [];
+  subscription: Subscription;
+  @Input() fConf: Conf;
+  conf: Conf;
 
-  constructor(private newConfService: NewConfService,
-              private router: Router, private r:ActivatedRoute) { }
+  constructor(private managerService: ManagerService,
+              private router: Router, private r: ActivatedRoute) { }
 
   ngOnInit() {
-    this.data = this.fConf.program;
-    // console.log("data: "+ JSON.stringify(data));
-    let topics = [];
-    let tLength = this.fConf.main_topics.length;
-    for (let tIndex = 0; tIndex < tLength; tIndex++){//init topics array
+    // this.subscription = this.managerService.selectedConf$
+    //   .subscribe(conf => this.conf = conf);
+    // if (this.conf) {
+    //   console.log('conf: ' + JSON.stringify(this.conf.program));
+    //   this.data = this.conf.program;
+    // }
+    // console.log('fConf: ' + JSON.stringify(this.fConf.program));
+    Object.assign(this.data, this.fConf.program);
+    this.conf = <Conf>this.deepCopy(this.fConf);
+    const topics = [];
+    const tLength = this.fConf.main_topics.length;
+    for (let tIndex = 0; tIndex < tLength; tIndex++) {// init topics array
       topics.push(this.fConf.main_topics[tIndex]);
       this.topicsLectures[tIndex] = [];
     }
-    console.log("topicsLectures: " + JSON.stringify(this.topicsLectures));
-    console.log("topics: " + JSON.stringify(topics));
-    for (let i = 0; i < this.data.length; i++){//loop over days
-      for(let j = 0; j < this.data[i].length; j++){//loop over session
-        for (let tIndex = 0; tIndex < tLength; tIndex++){//loop over topic
+    console.log('topicsLectures: ' + JSON.stringify(this.topicsLectures));
+    console.log('topics: ' + JSON.stringify(topics));
+    for (let i = 0; i < this.data.length; i++) {// loop over days
+      for (let j = 0; j < this.data[i].length; j++) {// loop over session
+        for (let tIndex = 0; tIndex < tLength; tIndex++) {// loop over topic
           this.topicsLectures[tIndex] = this.data[i][j].lectures.filter(lct => lct.topic === topics[tIndex]);
           // console.log("data[i][j]: "+ JSON.stringify(this.topicsLectures));
         }
         // console.log("topicsLectures: " + JSON.stringify(this.topicsLectures));
         this.data[i][j].lectures = [];
-        for (let tIndex = 0; tIndex < tLength; tIndex++){
+        for (let tIndex = 0; tIndex < tLength; tIndex++) {
           this.data[i][j].lectures[tIndex] = this.topicsLectures[tIndex];
         }
       }
     }
-    // console.log("super data: " + JSON.stringify(this.data));
-    //
-    // this.newConfService.newConf.subscribe((data:any)=>{
-    //   this.data = data.program;
-    //   // console.log("data: "+ JSON.stringify(data));
-    //   let topics = [];
-    //   let tLength = data.main_topics.length;
-    //   for (let tIndex = 0; tIndex < tLength; tIndex++){//init topics array
-    //     topics.push(data.main_topics[tIndex]);
-    //     this.topicsLectures[tIndex] = [];
-    //   }
-    //   console.log("topicsLectures: " + JSON.stringify(this.topicsLectures));
-    //   console.log("topics: " + JSON.stringify(topics));
-    //   for (let i = 0; i < this.data.length; i++){//loop over days
-    //     for(let j = 0; j < this.data[i].length; j++){//loop over session
-    //       for (let tIndex = 0; tIndex < tLength; tIndex++){//loop over topic
-    //         this.topicsLectures[tIndex] = this.data[i][j].lectures.filter(lct => lct.topic === topics[tIndex]);
-    //         // console.log("data[i][j]: "+ JSON.stringify(this.topicsLectures));
-    //       }
-    //       // console.log("topicsLectures: " + JSON.stringify(this.topicsLectures));
-    //       this.data[i][j].lectures = [];
-    //       for (let tIndex = 0; tIndex < tLength; tIndex++){
-    //         this.data[i][j].lectures[tIndex] = this.topicsLectures[tIndex];
-    //       }
-    //     }
-    //   }
-    //   console.log("super data: " + JSON.stringify(this.data));
-    // });
+    // console.log('fConf: ' + JSON.stringify(this.fConf.program));
   }
+  deepCopy(obj) {
+    let copy;
 
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || 'object' !== typeof obj) { return obj; }
+
+    // Handle Date
+    if (obj instanceof Date) {
+      copy = new Date();
+      copy.setTime(obj.getTime());
+      return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+      copy = [];
+      for (let i = 0, len = obj.length; i < len; i++) {
+        copy[i] = this.deepCopy(obj[i]);
+      }
+      return copy;
+    }
+
+    // Handle Object
+    if (obj instanceof Object) {
+      copy = {};
+      for (const attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = this.deepCopy(obj[attr]);
+      }
+      return copy;
+    }
+
+    throw new Error('Unable to copy obj! Its type isn\'t supported.');
+  }
 }
--- src/app/new-conf-program/new-conf-program.component.ts
+++ src/app/new-conf-program/new-conf-program.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import { NewConfService } from '../services/newConf.service';
 import { ConfSession } from '../models/confSession';
 import {NgForm} from '@angular/forms';
@@ -7,6 +7,9 @@ import {Conf} from '../models/conf';
 import {Lecture} from '../models/lecture';
 import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 import {Subscription} from 'rxjs/Subscription';
+import {EditConfSessionComponent} from '../edit-conf-session/edit-conf-session.component';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {ManagerService} from '../services/manager.service';
 
 @Component({
   selector: 'app-new-conf-program',
@@ -19,52 +22,47 @@ export class NewConfProgramComponent implements OnInit {
   numDays: number;
   conf: Conf;
   lectures: Lecture[] = [];
-  data: any = [];
+  dataProg: any = [];
   avialble= true;
   subscription: Subscription;
   newConf: Conf;
 
   constructor(private newConfService: NewConfService,
+              private managerService: ManagerService,
               private router: Router, private r: ActivatedRoute,
+              public dialogRef: MatDialogRef<EditConfSessionComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any,
               private spinnerService: Ng4LoadingSpinnerService) { }
 
   ngOnInit() {
-    this.subscription = this.newConfService.newConf$
-      .subscribe(conf => this.newConf = conf);
-    console.log('new conf: ' + JSON.stringify(this.newConf));
-    this.confId = this.newConf._id;
-    if (!this.confId) {
-      console.log('no new conf');
-      this.confId = '5aeb7d196226470004135c4c';
-      this.newConfService.getConfById(this.confId).then((conf) => {
-        this.newConf = conf;
-        // console.log("new conf: " + JSON.stringify(this.newConf));
-        this.numDays = this.newConf.duration;
-      });
-    }
-    else {
-      this.numDays = this.newConf.duration;
+    this.numDays = this.data.duration;
+    this.confId = this.data._id;
+    // console.log('data: ' + JSON.stringify(this.data.program[0][0].lectures));
+    if (this.data.program[0][0].lectures.length > 0) {
+      this.conf = this.data;
+      this.managerService.setSelectedConf(this.conf);
+      this.avialble = false;
     }
   }
   counter(i: number) {
     return new Array(i);
   }
 
-  buildProgram(){
-    for (let i = 0; i < this.numDays; i++){
-      this.data[i] = [];
+  buildProgram() {
+    for (let i = 0; i < this.numDays; i++) {
+      this.dataProg[i] = [];
     }
     this.spinnerService.show();
     this.newConfService.buildProgram(this.confId).then((confSession) => {
       this.confSession = confSession;
       // console.log(this.confSession);
-      // console.log("data: "+ JSON.stringify(this.data));
+      // console.log("dataProg: "+ JSON.stringify(this.dataProg));
       for (let i = 0; i < this.confSession.length; i++) { // split confSessions by day
-        this.data[this.confSession[i].dayNum - 1].push(this.confSession[i]);
+        this.dataProg[this.confSession[i].dayNum - 1].push(this.confSession[i]);
       }
       this.newConfService.getConfById(this.confId).then((conf) => {
         this.conf = conf;
-        this.conf.program = this.data;
+        this.conf.program = this.dataProg;
         this.spinnerService.hide();
         console.log('conf1: ' + JSON.stringify(this.conf));
         // this.newConfService.newConf.emit(this.conf);
@@ -72,5 +70,8 @@ export class NewConfProgramComponent implements OnInit {
         this.avialble = false;
       });
     });
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
